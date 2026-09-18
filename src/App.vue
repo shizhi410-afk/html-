e865193932a7e68f32a7ca88bee539b5e5d5596c
世凪 <3277586737@qq.com>
2026-09-18 21:50:39 +0800

    初始化 vue_test 项目

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="app">
+        <H1>
+            HELLO
+        </H1>
+    </div>
+</template>    
+
+<script lang="JS">
+    export default{
+        name:'App'
+    }
+</script>
+
+<style>
+    .app{
+        background-color: #ddd;
+        box-shadow: 0 0 10px;
+        border-radius: 10px;
+        padding: 20px;
+    }
+</style>
